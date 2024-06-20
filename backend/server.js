9acb19b3460e2916409e93d0df2df5062c7cf33b
KovacevicAleksa <aleksakovacevicc@gmail.com>
2024-06-20 14:46:03 +0200

    Added express-rate-limit

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const path = require("path");
+const rateLimit = require("express-rate-limit");
+
 const Account = require("./models/account");
 const Event = require("./models/event");
 
@@ -15,6 +17,15 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Set up a general rate limiter for all requests
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again after 15 minutes",
+});
+
+app.use(limiter);
 
 //REgistrovanje
 app.post("/register", async (req, res) => {
@@ -149,15 +160,14 @@ app.patch("/edit/events/:id", async (req, res) => {
   }
 });
 
-// rditovanje naloga preko id
+// Edit account with ID
 app.patch("/edit/account/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const { events, ...otherUpdates } = req.body;
 
-    const update = { $push: { events } }; // Dodavanje novih eventa
+    const update = { $push: { events } };
 
-    //Dodavanje novih updatova
     if (Object.keys(otherUpdates).length > 0) {
       update.$set = otherUpdates;
     }
