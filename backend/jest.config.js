a96c0078fbd8ac0a615d20065a6c769b1dc84021
KovacevicAleksa <aleksakovacevicc@gmail.com>
2024-09-07 17:04:17 +0200

    Fix Jest configuration for ES modules in backend

---
+++ backend/jest.config.js
@@ -0,0 +1,8 @@
+export default {
+  transform: {},
+  moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node"],
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+};
