5d1b9ac74c8280c5e02a4ae293c4c29120d267a4
User <aleksakovacevicc@gmail.com>
2024-11-06 20:55:56 +0100

    edited /health route

--- backend/routes/healthCheckRoutes.js
+++ backend/routes/healthCheckRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
+import Event from "../models/event.js";
 import pkg from "pg";
 import dotenv from "dotenv";
 
-const { Pool } = pkg; // Destructuring Pool from pg package
+dotenv.config();
 
+const { Pool } = pkg;
 const router = express.Router();
+
 
 const pool = new Pool({
   host: process.env.PG_HOST || "postgres",
@@ -14,21 +17,26 @@ const pool = new Pool({
   port: 5432,
 });
 
+
 router.get("/health", async (req, res) => {
-  res.status(200).json({ status: "healthy" });
+  try {
+    await pool.query("SELECT 1");
+    const events = await Event.find({});
+
+    res.status(200).json({ status: "OK", message: "PostgreSQL and MongoDB connection is healthy" });
+  } catch (error) {
+    console.error("Health check failed:", error);
+    res.status(500).json({ status: "unhealthy", error: error.message });
+  }
 });
 
 router.get("/healthcheck/pg", async (req, res) => {
   try {
-    await pool.query("SELECT 1"); // Simple query to check database connection
-    res
-      .status(200)
-      .json({ status: "OK", message: "Database connection is healthy" });
+    await pool.query("SELECT 1");
+    res.status(200).json({ status: "OK", message: "Database connection is healthy" });
   } catch (error) {
     console.error("Database health check failed:", error);
-    res
-      .status(500)
-      .json({ status: "ERROR", message: "Database connection failed" });
+    res.status(500).json({ status: "ERROR", message: "Database connection failed" });
   }
 });
 
